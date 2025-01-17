6a907c43a280f980398e36721b78d6f4027b59e6
akainett <danettey9@gmail.com>
2025-01-16 23:56:50 -0800

    wrote a loop to return a string number of times but not negative

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0) {
+    return "ERROR";
+    }
+    
+    let results = "";
+    for (let i = 0; i < num; i++) {
+        results += string;  
+    }
+    return results;
 };
-
+repeatString('hey', 3);
 // Do not edit below this line
 module.exports = repeatString;
