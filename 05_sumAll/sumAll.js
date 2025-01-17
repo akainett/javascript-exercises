fea85579382b11ba102ecd4422164333a56f17e0
akainett <danettey9@gmail.com>
2025-01-17 03:15:47 -0800

    add all numbers between two integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (min <= 0 || max <= 0 || !Number.isInteger(min) || !Number.isInteger(max)) {
+        return "ERROR"
+    }
+    let sum = 0;
+    
+    if( min > max) {
+        const cop = min
+        min = max
+        max = cop 
+    }
+    
+    for (let i = min; i <= max; i++) {
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
