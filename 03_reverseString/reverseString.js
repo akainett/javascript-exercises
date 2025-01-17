21f58cc8b0d9706136e104bfa8ec5066614cc648
akainett <danettey9@gmail.com>
2025-01-17 00:35:45 -0800

    reversed a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split("").reverse(string).join("");
 };
-
+reverseString("hello there")
 // Do not edit below this line
 module.exports = reverseString;
