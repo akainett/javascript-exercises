2441ece528cfbbddef844552c297c628a70fcbd8
akainett <danettey9@gmail.com>
2025-01-17 17:41:18 -0800

    corrected naming to camel style

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -2,8 +2,8 @@ const convertToCelsius = function(tempInFahrenheit) {
   return Math.round((tempInFahrenheit - 32) * (5/9) *10)/10
 };
 
-const convertToFahrenheit = function(tempIncelsius) {
-  return Math.round(((tempIncelsius * (9/5)) + 32) * 10)/10
+const convertToFahrenheit = function(tempInCelsius) {
+  return Math.round(((tempInCelsius * (9/5)) + 32) * 10)/10
 };
 
 // Do not edit below this line
