a1183b4ebb2845b5c0cc8c33f1f53e929a22997f
akainett <danettey9@gmail.com>
2025-01-27 21:24:29 -0800

    solved assignment on  palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+ 
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const filteredString = string
+        .toLowerCase()
+        .split('')
+        .filter(char => alphanumerical.includes(char))
+        .join('')
+    
+    const reversedString = filteredString.split('').reverse().join('')
+
+    return filteredString === reversedString
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
