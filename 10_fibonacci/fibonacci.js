78a0c5ae4355bb6ec7299ad12a6419fb4e8fb6a3
akainett <danettey9@gmail.com>
2025-01-27 23:01:42 -0800

    solved fibonacci assignment

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let num;
+    if (typeof n === 'string') {
+        num = parseInt(n)
+    } else {
+        num = n
+    }    
+      
+    if (n < 0) return 'OOPS'
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    
+    // Initialize the first two numbers of the sequence
+    let prev = 0, curr = 1;
+    
+    for (let i = 2; i <= n; i++) {
+        let next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+    
+    return curr;
 };
 
 // Do not edit below this line
