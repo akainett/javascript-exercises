3102946dcb5762e79be670c88b20b8b359ad8bfb
akainett <danettey9@gmail.com>
2025-01-28 13:48:41 -0800

    solved assignment 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const calcAge = function(person) {
+    if(!person.yearOfDeath) {
+       person.yearOfDeath = new Date().getFullYear()
+    }
+    return person.yearOfDeath - person.yearOfBirth
+}
 
+const findTheOldest = function(people) {
+    const oldestToYoungest =  people.sort((currentPerson, nextPerson) => calcAge(nextPerson) - calcAge(currentPerson))
+    
+    const oldestPerson = oldestToYoungest[0];
+    return oldestPerson;
 };
 
 // Do not edit below this line
